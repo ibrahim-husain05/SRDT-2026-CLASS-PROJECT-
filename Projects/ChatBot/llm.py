78dcab605fc9efb4645c00939790b78b23de6653
ibrahim-husain05 <ibrahimhusain059@gmail.com>
2026-07-13 15:33:55 +0530

    Delete Projects directory

--- Projects/ChatBot/llm.py
+++
@@ -1,8 +0,0 @@
-from groq import Groq
-client=Groq(api_key="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")
-def ask_llm(messages):
-    response=client.chat.completions.create(
-        model="llama-3.1-8b-instant",
-        messages=messages
-    )
-    return response.choices[0].message.content
--- Projects/ChatBot/memory.py
+++
@@ -1,6 +0,0 @@
-def add_memory(messages,role,content):
-    messages.append({
-        "role":role,
-        "content": content
-    })
-    return messages
